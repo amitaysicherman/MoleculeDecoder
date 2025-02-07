4d351557a5603c55a0340ec3f8dac46fe18dd774
amitay.s <amitay.sich@gmail.com>
2025-02-07 12:30:54 +0200

    start!

---
+++ get_all_uspto_mols.py
@@ -0,0 +1,11 @@
+all_mols = []
+base_dir="USPTO"
+for split in ["train","valid","test"]:
+    for side in ["src","tgt"]:
+        with open(f"{base_dir}/{side}-{split}.txt") as f:
+            lines = f.read().splitlines()
+        mols = [line.replace(" ", "").split(".") for line in lines]
+        all_mols.extend([m for mol in mols for m in mol])
+print(len(all_mols))
+with open("all_mols.txt", "w") as f:
+    f.write("\n".join(all_mols))
