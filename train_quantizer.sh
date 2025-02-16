99a5e66b73b70309747e6d2a1154a69e5d5929d7
amitay.s <amitay.sich@gmail.com>
2025-02-16 21:41:15 +0200

    start!

--- train_quantizer.sh
+++ train_quantizer.sh
@@ -4,17 +4,17 @@
 #SBATCH --requeue
 #SBATCH -c 8
 #SBATCH --gres=gpu:L40:1
-#SBATCH --array=1-18
+#SBATCH --array=0-17
 
 # configs: num_quantizers, codebook_size
 num_quantizers_list=(8 8 8 32 32 32 64 64 64)
 codebook_size_list=(256 512 1024 256 512 1024 256 512 1024)
 
-config_index=$(($SLURM_ARRAY_TASK_ID - 1))
+config_index=$(($SLURM_ARRAY_TASK_ID))
 # if config index > 9, then we need to subtract 9 from it and set learning_rate to 0.01
 if [ $config_index -ge 8 ];
 then
-    config_index=$(($config_index - 8))
+    config_index=$(($config_index - 9))
     learning_rate=0.01
 else
     learning_rate=0.001
