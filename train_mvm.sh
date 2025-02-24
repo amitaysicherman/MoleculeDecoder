08d00914e8b5e39f66de4203271d32e8b7655ff6
amitay.s <amitay.sich@gmail.com>
2025-02-24 16:21:34 +0200

    start!

--- train_mvm.sh
+++ train_mvm.sh
@@ -13,4 +13,4 @@ config_index=$(($SLURM_ARRAY_TASK_ID))
 size=${sizes_list[$config_index]}
 learning_rate=${lr_list[$config_index]}
 echo "size: $size, learning_rate: $learning_rate, config_index: $config_index"
-python train_mvm.py --size $size --learning_rate $learning_rate
+python train_mvm.py --size $size --lr $learning_rate
