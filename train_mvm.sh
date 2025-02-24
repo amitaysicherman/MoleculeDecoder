f19749747e8829b0676f413e34ffcd37296c2235
amitay.s <amitay.sich@gmail.com>
2025-02-24 16:11:27 +0200

    start!

---
+++ train_mvm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=7-00
+#SBATCH --mem=64G
+#SBATCH --requeue
+#SBATCH -c 8
+#SBATCH --gres=gpu:L40:1
+#SBATCH --array=0-14
+
+sizes_list=("xs" "xs" "xs" "s" "s" "s" "m" "m" "m" "l" "l" "l" "xl" "xl" "xl")
+lr_list=(1e-3 1e-4 1e-5 1e-3 1e-4 1e-5 1e-3 1e-4 1e-5 1e-3 1e-4 1e-5 1e-3 1e-4 1e-5)
+
+config_index=$(($SLURM_ARRAY_TASK_ID))
+size=${sizes_list[$config_index]}
+learning_rate=${lr_list[$config_index]}
+echo "size: $size, learning_rate: $learning_rate, config_index: $config_index"
+python train_mvm.py --size $size --learning_rate $learning_rate
