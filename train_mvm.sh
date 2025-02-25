61ea3f99bb61325bb6ec9b28a92a3e3e50cdbc64
amitay.s <amitay.sich@gmail.com>
2025-02-25 10:44:43 +0200

    start!

--- train_mvm.sh
+++ train_mvm.sh
@@ -3,19 +3,12 @@
 #SBATCH --mem=64G
 #SBATCH --requeue
 #SBATCH -c 8
-#SBATCH --gres=gpu:L40:1
-#SBATCH --array=0-26
+#SBATCH --gres=gpu:L40:4
+#SBATCH --array=0-1
 
-sizes_list=('m' 'm' 'm' 'm' 'm' 'm' 'm' 'm' 'm' 's' 's' 's' 's' 's' 's' 's' 's' 's' 'l' 'l' 'l' 'l' 'l' 'l' 'l' 'l' 'l')
-lr_list=(0.0001 0.0001 0.0001 0.001 0.001 0.001 1e-05 1e-05 1e-05 0.0001 0.0001 0.0001 0.001 0.001 0.001 1e-05 1e-05 1e-05 0.0001 0.0001 0.0001 0.001 0.001 0.001 1e-05 1e-05 1e-05)
-alpha_list=(0 1 0.5 0 1 0.5 0 1 0.5 0 1 0.5 0 1 0.5 0 1 0.5 0 1 0.5 0 1 0.5 0 1 0.5)
-
-
-
+sizes_list=('l' 'xl')
 
 config_index=$(($SLURM_ARRAY_TASK_ID))
 size=${sizes_list[$config_index]}
-learning_rate=${lr_list[$config_index]}
-alpha=${alpha_list[$config_index]}
-echo "size: $size, learning_rate: $learning_rate, config_index: $config_index"
-python train_mvm.py --size $size --lr $learning_rate --alpha $alpha
+echo "size: $size config_index: $config_index"
+python train_mvm.py --size $size
