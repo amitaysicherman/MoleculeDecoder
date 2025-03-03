191393a8c35553ce1a18bb88045d6d56c7f96f37
amitay.s <amitay.sich@gmail.com>
2025-03-03 12:35:24 +0200

    start!

--- autoencoder/data.py
+++ autoencoder/data.py
@@ -16,8 +16,6 @@ RDLogger.DisableLog('rdApp.*')
 
 tokenizer_file = "pubchem-canonical/tokenizer/"
 
-SMILES_TOKENIZER_PATTERN = r"(\%\([0-9]{3}\)|\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\||\(|\)|\.|=|#|-|\+|\\|\/|:|~|@|\?|>>?|\*|\$|\%[0-9]{2}|[0-9])"
-SMILES_REGEX = re.compile(SMILES_TOKENIZER_PATTERN)
 
 
 def preprocess_smiles(smiles: str) -> str:
@@ -29,6 +27,9 @@ def preprocess_smiles(smiles: str) -> str:
 
 
 def smiles_to_tokens(smiles: str) -> list:
+    SMILES_TOKENIZER_PATTERN = r"(\%\([0-9]{3}\)|\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\||\(|\)|\.|=|#|-|\+|\\|\/|:|~|@|\?|>>?|\*|\$|\%[0-9]{2}|[0-9])"
+    SMILES_REGEX = re.compile(SMILES_TOKENIZER_PATTERN)
+
     # canonicalize SMILES and remove stereochemistry
     smiles = preprocess_smiles(smiles)
     tokens = [token for token in SMILES_REGEX.findall(smiles)]
