ba58455022a42e467015592266af0f06c37be2da
amitay.s <amitay.sich@gmail.com>
2025-03-03 09:54:50 +0200

    start!

--- autoencoder/data.py
+++ autoencoder/data.py
@@ -46,9 +46,10 @@ def get_tokenizer(input_file="pubchem-canonical/CID-SMILES-CANONICAL.smi"):
 
     counter = Counter()
     with open(input_file, 'r', encoding='utf-8') as f:
-        with ProcessPoolExecutor() as executor:
-            for tokens in tqdm(executor.map(process_line, f)):
-                counter.update(tokens)
+        lines = f.readlines()
+    with ProcessPoolExecutor() as executor:
+        for tokens in tqdm(executor.map(process_line, lines), total=len(lines)):
+            counter.update(tokens)
 
     vocab = {"<pad>": 0, "<unk>": 1, "<bos>": 2, "<eos>": 3}
     idx = len(vocab)
