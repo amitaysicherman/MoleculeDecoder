e63c0d7451bcd0c65ccc179e80ecb2045e63ad43
amitay.s <amitay.sich@gmail.com>
2025-03-08 20:35:06 +0200

    start!

--- autoencoder/train_mvm.py
+++ autoencoder/train_mvm.py
@@ -384,8 +384,8 @@ def main(batch_size=1024, num_epochs=10, lr=1e-4, size="m", alpha=0.5, use_molfo
     #     torch.load(f"{get_last_cp(f'res_auto_mvm/{output_suf}')}/pytorch_model.bin", map_location=device))
     #
     # print(trainer.evaluate())
-    trainer.train(resume_from_checkpoint=get_last_cp(f"res_auto_mvm/{output_suf}") is not None)
-
+    # trainer.train(resume_from_checkpoint=get_last_cp(f"res_auto_mvm/{output_suf}") is not None)
+    trainer.train()
 
 if __name__ == "__main__":
     import argparse
